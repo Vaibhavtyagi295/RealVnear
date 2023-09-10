1dd359a134fa682cf3ca75210b3291b083b32de4
vaibhav tyagi <vaibhavtyagi295@gmail.com>
2023-09-10 16:28:02 +0530

    Update users.js

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const plm = require("passport-local-mongoose");
 
 mongoose.set('strictQuery', true);
-mongoose.connect("mongodb://localhost/gojo")
+mongoose.connect("mongodb+srv://vaibhav:aman@cluster0.il74hmr.mongodb.net/VVnear?retryWrites=true&w=majority")
   .then(function () {
     console.log("Connected to the database");
   });
